241f050d40378137d419d29764cda7afb9104e50
ebyoupa <byoungmok.park@ericsson.com>
2017-02-28 15:34:28 +0900

    new test

--- src/main/java/cafe/dev/test_git/App.java
+++ src/main/java/cafe/dev/test_git/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!!-tset version" );
+        System.out.println( "Hello World!!-tset version -- new test go" );
     }
 }
