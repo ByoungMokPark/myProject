ac21f8e14bc285b5dcf849bf0ae54092d772fb95
ebyoupa <byoungmok.park@ericsson.com>
2017-03-10 19:24:00 +0900

    maset edit ss Conflicts: 	src/main/java/cafe/dev/test_git/App.java

--- src/main/java/cafe/dev/test_git/App.java
+++ src/main/java/cafe/dev/test_git/App.java
@@ -19,7 +19,8 @@ public class App
         System.out.println( "Hello World!!-tset version working 1231234k123123 file add 123" );
         System.out.println( "master add source add testbranch add master ad" );
         System.out.println( "master add commit" );
-        System.out.println( "master add commit2 edit1" );
+        System.out.println( "master add commit2 edit1 12" );
+        System.out.println("test");
     }
    
 }
