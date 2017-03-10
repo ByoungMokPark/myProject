5706878884b91732ebab8d92c3ea0105c4c863d7
ebyoupa <byoungmok.park@ericsson.com>
2017-03-10 16:58:01 +0900

    new file add source

--- src/main/java/cafe/dev/test_git/App_test.java
+++ src/main/java/cafe/dev/test_git/App_test.java
@@ -4,11 +4,11 @@ package cafe.dev.test_git;
  * Hello world!
  *
  */
-public class App 
+public class App_test 
 {
     public static void main( String[] args )
     {
-        System.out.println( "newfile add source" );
+        System.out.println( "Hello World!!-tset version" );
         System.out.println( "Hello World!!-tset version" );
         System.out.println( "Hello World!!-tset version -- new test go" );
         System.out.println( "Hello World!!-tset version working" );
