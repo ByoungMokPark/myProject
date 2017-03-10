f5bcd9ff0baa9089da7d3e8411252b3229eb2c20
ebyoupa <byoungmok.park@ericsson.com>
2017-03-10 19:27:26 +0900

    master 1

--- src/main/java/cafe/dev/test_git/App.java
+++ src/main/java/cafe/dev/test_git/App.java
@@ -19,7 +19,7 @@ public class App
         System.out.println( "Hello World!!-tset version working 1231234k123123 file add 123" );
         System.out.println( "master add source add testbranch add master ad" );
         System.out.println( "master add commit" );
-        System.out.println( "master add commit2 edit" );
+        System.out.println( "master add commit2 edit1" );
     }
    
 }
