a0c1d6382db9ecface675c17b13023703580ef8a
ebyoupa <byoungmok.park@ericsson.com>
2017-03-10 17:55:35 +0900

    maser

--- src/main/java/cafe/dev/test_git/App.java
+++ src/main/java/cafe/dev/test_git/App.java
@@ -18,12 +18,9 @@ public class App
         System.out.println( "Hello World!!-tset version working 1231234k123" ); 
         System.out.println( "Hello World!!-tset version working 1231234k123123 file add 123" );
         System.out.println( "master add source add testbranch add master ad" );
-<<<<<<< Upstream, based on master
         System.out.println( "master add commit" );
         System.out.println( "master add commit2" );
-=======
         System.out.println( "master add commit 22" );
->>>>>>> df37d1b reabase
     }
    
 }
