1972b0c649bfcea853a5c0f999b8c088b2dcd95f
ebyoupa <byoungmok.park@ericsson.com>
2017-03-10 16:56:47 +0900

    masert add source

--- src/main/java/cafe/dev/test_git/App.java
+++ src/main/java/cafe/dev/test_git/App.java
@@ -16,7 +16,8 @@ public class App
         System.out.println( "Hello World!!-tset version working 1231234k123" );
         System.out.println( "Hello World!!-tset version working 1231234k123" ); 
         System.out.println( "Hello World!!-tset version working 1231234k123" ); 
-        System.out.println( "Hello World!!-tset version working 1231234k123123 file add 123" ); 
+        System.out.println( "Hello World!!-tset version working 1231234k123123 file add 123" );
+        System.out.println( "master add source" ); 
     }
    
 }
