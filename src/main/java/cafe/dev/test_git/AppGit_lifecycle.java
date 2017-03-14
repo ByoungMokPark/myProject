9dc2235fa9ff651dfc3c65433ec2832a7ae41cfe
ebyoupa <byoungmok.park@ericsson.com>
2017-03-14 16:16:17 +0900

    staged

---
+++ src/main/java/cafe/dev/test_git/AppGit_lifecycle.java
@@ -0,0 +1,22 @@
+package cafe.dev.test_git;
+
+/**
+ * Hello world!
+ *
+ */
+public class AppGit_lifecycle 
+{
+    public static void main( String[] args )
+    {
+    	// 추가 123
+        System.out.println( "Hello World!!-tset version" );
+        System.out.println( "Hello World!!-tset version working" );
+        
+        System.out.println( "Hello World!!-tset version working adf"  );
+        
+        System.out.println( "Hello World!!-tset version working" );
+        System.out.println( "Hello World!!-tset version working 123" );
+        System.out.println( "Hello World!!-tset version working 12 3 12121 as"  );
+    }
+}
+
